c5436ebf39c0ddadb2d7014b29a3338a5ffb4876
Simon Walker <s.r.walker101@googlemail.com>
2019-09-09 18:26:11 +0100

    Add initial webpack file

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.elm$/,
+      exclude: [/elm-stuff/, /node_modules/],
+      use: {
+        loader: 'elm-webpack-loader',
+        options: {},
+      },
+    }],
+  },
+};
