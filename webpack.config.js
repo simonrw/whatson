02f8a50f6913869c8a1da9a193e9a34f7ae3c99e
Simon Walker <s.r.walker101@googlemail.com>
2019-09-10 22:10:31 +0100

    Include tailwind package

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,41 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "whatson/static/js"),
     filename: "bundle.js",
   },
   module: {
-    rules: [{
-      test: /\.elm$/,
-      exclude: [/elm-stuff/, /node_modules/],
-      use: {
-        loader: 'elm-webpack-loader',
-        options: {
-          debug: true,
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {
+            debug: true,
+          },
         },
       },
-    }],
+      {
+        test: /\.css$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [
+          'style-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+            },
+          },
+        ],
+      }
+    ],
   },
 };
