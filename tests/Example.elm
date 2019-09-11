a346970e133e3b78cfa11e798e59732838854c3b
Simon Walker <s.r.walker101@googlemail.com>
2019-09-11 22:00:52 +0100

    Test initial page state

--- tests/Example.elm
+++ tests/Example.elm
@@ -2,9 +2,25 @@ module Example exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Main
+import ProgramTest exposing (ProgramTest, expectViewHas)
 import Test exposing (..)
+import Test.Html.Selector exposing (text)
+
+
+start : ProgramTest Main.Model Main.Msg (Cmd Main.Msg)
+start =
+    ProgramTest.createElement
+        { init = Main.init
+        , update = Main.update
+        , view = Main.view
+        }
+        |> ProgramTest.start ()
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    test "initial state" <|
+        \() ->
+            start
+                |> expectViewHas [ text "What's on?" ]
