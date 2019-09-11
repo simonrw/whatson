7c41cd0a633619d318c7a592890b4a16884451f5
Simon Walker <s.r.walker101@googlemail.com>
2019-09-11 22:21:22 +0100

    Update test for extractors

--- testing/test_extractors.py
+++ testing/test_extractors.py
@@ -1,20 +1,22 @@
 import vcr
-from whatson.ingest import parse_belgrade, parse_albany
+from whatson.ingest import ParseBelgrade, ParseAlbany
 
 
 @vcr.use_cassette("fixtures/test_parse_belgrade.yaml")
 def test_parse_belgrade():
+    root_url = "http://www.belgrade.co.uk/"
     url = "http://www.belgrade.co.uk/whats-on/"
 
-    results = list(parse_belgrade(url))
+    results = list(ParseBelgrade(url, root_url).parse())
 
     assert len(results) == 66
 
 
 @vcr.use_cassette("fixtures/test_parse_albany.yaml")
 def test_parse_albany():
+    root_url = "https://albanytheatre.co.uk/"
     url = "https://albanytheatre.co.uk/whats-on/"
 
-    results = list(parse_albany(url))
+    results = list(ParseAlbany(url, root_url).parse())
 
     assert len(results) == 49
