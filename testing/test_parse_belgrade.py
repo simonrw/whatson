f89fc736996cf4e9de05e02a815f1e1b14332f5a
Simon Walker <s.r.walker101@googlemail.com>
2019-09-12 22:53:11 +0100

    Update belgrade tests

--- testing/test_parse_belgrade.py
+++ testing/test_parse_belgrade.py
@@ -12,5 +12,19 @@ class TestParseBelgrade:
 
         self.results = list(ParseBelgrade(url, root_url).parse())
 
-    def test_parse_belgrade(self):
-        assert len(self.results) == 66
+    def test_number_of_results(self):
+        assert len(self.results) == 64
+
+    @pytest.mark.parametrize(
+        "idx,start,end",
+        [
+            (0, date(2019, 9, 13), date(2019, 9, 13)),
+            (9, date(2019, 10, 2), date(2019, 10, 2)),
+            (28, date(2019, 10, 29), date(2019, 11, 2)),
+            (42, date(2020, 1, 15), date(2020, 1, 15)),
+            (-1, date(2020, 7, 27), date(2020, 7, 27)),
+        ],
+    )
+    def test_parse_dates(self, idx, start, end):
+        assert self.results[idx].start_date == start
+        assert self.results[idx].end_date == end
