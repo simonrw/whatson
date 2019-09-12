438af338b708498b9e9ae055f6079145569de92d
Simon Walker <s.r.walker101@googlemail.com>
2019-09-12 22:45:48 +0100

    Handle albany date extraction

--- testing/test_extractors.py
+++ testing/test_extractors.py
@@ -1,5 +1,7 @@
+import pytest
 import vcr
 from whatson.ingest import ParseBelgrade, ParseAlbany
+from datetime import date
 
 
 @vcr.use_cassette("fixtures/test_parse_belgrade.yaml")
@@ -12,11 +14,26 @@ def test_parse_belgrade():
     assert len(results) == 66
 
 
-@vcr.use_cassette("fixtures/test_parse_albany.yaml")
-def test_parse_albany():
-    root_url = "https://albanytheatre.co.uk/"
-    url = "https://albanytheatre.co.uk/whats-on/"
+class TestParseAlbany(object):
+    @vcr.use_cassette("fixtures/test_parse_albany.yaml")
+    def setup(self):
+        root_url = "https://albanytheatre.co.uk/"
+        url = "https://albanytheatre.co.uk/whats-on/"
 
-    results = list(ParseAlbany(url, root_url).parse())
+        self.results = list(ParseAlbany(url, root_url).parse())
 
-    assert len(results) == 49
+    def test_number_of_results(self):
+        assert len(self.results) == 49
+
+    @pytest.mark.parametrize(
+        "idx,start,end",
+        [
+            (0, date(2019, 9, 13), date(2019, 9, 13)),
+            (1, date(2019, 9, 13), date(2019, 9, 22)),
+            (37, date(2020, 1, 1), date(2020, 1, 5)),
+            (48, date(2020, 6, 6), date(2020, 6, 6)),
+        ],
+    )
+    def test_parse_dates(self, idx, start, end):
+        assert self.results[idx].start_date == start
+        assert self.results[idx].end_date == end
