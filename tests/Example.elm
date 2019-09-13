cfeb23805b5638a88b9ce0b454113ca347086c44
Simon Walker <s.r.walker101@googlemail.com>
2019-09-13 17:19:07 +0100

    Add sorting

--- tests/Example.elm
+++ tests/Example.elm
@@ -3,37 +3,71 @@ module Example exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Main
-import ProgramTest exposing (ProgramTest, expectHttpRequest, expectViewHas, selectOption)
 import Test exposing (..)
 import Test.Html.Selector exposing (text)
 
 
-start : ProgramTest Main.Model Main.Msg (Cmd Main.Msg)
-start =
-    ProgramTest.createElement
-        { init = Main.init
-        , update = Main.update
-        , view = Main.view
-        }
-        |> ProgramTest.start ()
-
-
 suite : Test
 suite =
-    describe "Page state"
-        [ test "initial state" <|
+    describe "comparing shows by date"
+        [ test "comparing dates" <|
             \() ->
-                start
-                    |> expectViewHas [ text "What's on?" ]
-        , test "sepected initial HTTP request" <|
+                let
+                    a =
+                        { year = 2019
+                        , month = 10
+                        , day = 15
+                        }
+
+                    b =
+                        { year = 2019
+                        , month = 10
+                        , day = 16
+                        }
+
+                    sortValue =
+                        Main.compareDates a b
+                in
+                Expect.equal sortValue LT
+        , test "comparing shows" <|
             \() ->
-                start
-                    |> expectHttpRequest "GET"
-                        "/api/months"
-                        (.body >> Expect.equal """{"content":"updated"}""")
-        , test "selected option" <|
-            \() ->
-                start
-                    |> selectOption "select-time" "Choose a month" "14" "October 2019"
-                    |> expectViewHas [ text "What's on?" ]
+                let
+                    a =
+                        { name = "test"
+                        , theatre = "test"
+                        , imageUrl = ""
+                        , linkUrl = ""
+                        , startDate =
+                            { year = 2019
+                            , month = 10
+                            , day = 15
+                            }
+                        , endDate =
+                            { year = 2019
+                            , month = 10
+                            , day = 15
+                            }
+                        }
+
+                    b =
+                        { name = "test"
+                        , theatre = "test"
+                        , imageUrl = ""
+                        , linkUrl = ""
+                        , startDate =
+                            { year = 2019
+                            , month = 10
+                            , day = 16
+                            }
+                        , endDate =
+                            { year = 2019
+                            , month = 10
+                            , day = 15
+                            }
+                        }
+
+                    sortValue =
+                        Main.compareShowsByDate a b
+                in
+                Expect.equal sortValue LT
         ]
