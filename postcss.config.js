51c620a8bff6e7f790bf880263533b9fee83244e
Simon Walker <s.r.walker101@googlemail.com>
2019-09-22 23:05:35 +0100

    Set up purgecss to reduce bundle size

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    purgecss({
+      content: ['./whatson/templates/**/*.html', './src/**/*.js', './src/**/*.elm'],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ]
 }
 
