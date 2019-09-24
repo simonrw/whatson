686f29f062469597c476a62c1421b10ded37d724
Simon Walker <s.r.walker101@googlemail.com>
2019-09-25 00:11:06 +0100

    Update parsing tests to use pytest fixtures

--- testing/test_parse_belgrade.py
+++ testing/test_parse_belgrade.py
@@ -4,27 +4,29 @@ from whatson.ingest import ParseBelgrade, ParseAlbany
 from datetime import date
 
 
-class TestParseBelgrade:
-    @vcr.use_cassette("fixtures/test_parse_belgrade.yaml")
-    def setup(self):
-        root_url = "http://www.belgrade.co.uk/"
-        url = "http://www.belgrade.co.uk/whats-on/"
+@pytest.fixture(scope="session")
+@vcr.use_cassette("fixtures/test_parse_belgrade.yaml")
+def results():
+    root_url = "http://www.belgrade.co.uk/"
+    url = "http://www.belgrade.co.uk/whats-on/"
 
-        self.results = list(ParseBelgrade(url, root_url).parse())
+    return list(ParseBelgrade(url, root_url).parse())
 
-    def test_number_of_results(self):
-        assert len(self.results) == 64
 
-    @pytest.mark.parametrize(
-        "idx,start,end",
-        [
-            (0, date(2019, 9, 13), date(2019, 9, 13)),
-            (9, date(2019, 10, 2), date(2019, 10, 2)),
-            (28, date(2019, 10, 29), date(2019, 11, 2)),
-            (42, date(2020, 1, 15), date(2020, 1, 15)),
-            (-1, date(2020, 7, 27), date(2020, 7, 27)),
-        ],
-    )
-    def test_parse_dates(self, idx, start, end):
-        assert self.results[idx].start_date == start
-        assert self.results[idx].end_date == end
+def test_number_of_results(results):
+    assert len(results) == 64
+
+
+@pytest.mark.parametrize(
+    "idx,start,end",
+    [
+        (0, date(2019, 9, 13), date(2019, 9, 13)),
+        (9, date(2019, 10, 2), date(2019, 10, 2)),
+        (28, date(2019, 10, 29), date(2019, 11, 2)),
+        (42, date(2020, 1, 15), date(2020, 1, 15)),
+        (-1, date(2020, 7, 27), date(2020, 7, 27)),
+    ],
+)
+def test_parse_dates(results, idx, start, end):
+    assert results[idx].start_date == start
+    assert results[idx].end_date == end
--- testing/test_parse_symphony_hall.py
+++ testing/test_parse_symphony_hall.py
@@ -4,37 +4,40 @@ from whatson.ingest import ParseSymphonyHall
 from datetime import date
 
 
-class TestParseSymphonyHall(object):
-    @vcr.use_cassette("fixtures/test_parse_symphony_hall.yaml")
-    def setup(self):
-        root_url = "https://www.thsh.co.uk/"
-        url = "https://www.thsh.co.uk/whats-on/"
+@pytest.fixture(scope="session")
+@vcr.use_cassette("fixtures/test_parse_symphony_hall.yaml")
+def results():
+    root_url = "https://www.thsh.co.uk/"
+    url = "https://www.thsh.co.uk/whats-on/"
 
-        self.results = list(ParseSymphonyHall(url, root_url).parse())
+    return list(ParseSymphonyHall(url, root_url).parse())
 
-    def test_number_of_results(self):
-        assert len(self.results) == 348
 
-    @pytest.mark.parametrize(
-        "idx,start,end",
-        [
-            (0, date(2019, 9, 14), date(2019, 9, 14)),
-            (10, date(2019, 9, 21), date(2019, 12, 7)),
-            (198, date(2019, 12, 31), date(2020, 1, 2)),
-        ],
-    )
-    def test_parse_dates(self, idx, start, end):
-        assert self.results[idx].start_date == start
-        assert self.results[idx].end_date == end
+def test_number_of_results(results):
+    assert len(results) == 348
 
-    def test_no_duplicate_show_names(self):
-        """
-        Regression test. A bug in the code caused repeated finding of the
-        same title throughout the page. This test ensures this does not happen
-        again.
-        """
-        names = [show.name for show in self.results]
 
-        # Check that they are basically unique (some show names may be
-        # repeated, but certainly not too many
-        assert len(set(names)) == 316
+@pytest.mark.parametrize(
+    "idx,start,end",
+    [
+        (0, date(2019, 9, 14), date(2019, 9, 14)),
+        (10, date(2019, 9, 21), date(2019, 12, 7)),
+        (198, date(2019, 12, 31), date(2020, 1, 2)),
+    ],
+)
+def test_parse_dates(results, idx, start, end):
+    assert results[idx].start_date == start
+    assert results[idx].end_date == end
+
+
+def test_no_duplicate_show_names(results):
+    """
+    Regression test. A bug in the code caused repeated finding of the
+    same title throughout the page. This test ensures this does not happen
+    again.
+    """
+    names = [show.name for show in results]
+
+    # Check that they are basically unique (some show names may be
+    # repeated, but certainly not too many
+    assert len(set(names)) == 316
