ba73ea5635510ef4f682e8860558a280d3c84030
Simon Walker <s.r.walker101@googlemail.com>
2019-09-25 00:11:06 +0100

    Update parsing tests to use pytest fixtures

--- testing/test_parse_albany.py
+++ testing/test_parse_albany.py
@@ -1,29 +1,31 @@
 import pytest
 import vcr
-from whatson.ingest import ParseBelgrade, ParseAlbany
+from whatson.ingest import ParseAlbany
 from datetime import date
 
 
-class TestParseAlbany(object):
-    @vcr.use_cassette("fixtures/test_parse_albany.yaml")
-    def setup(self):
-        root_url = "https://albanytheatre.co.uk/"
-        url = "https://albanytheatre.co.uk/whats-on/"
+@pytest.fixture(scope="session")
+@vcr.use_cassette("fixtures/test_parse_albany.yaml")
+def results():
+    root_url = "https://albanytheatre.co.uk/"
+    url = "https://albanytheatre.co.uk/whats-on/"
 
-        self.results = list(ParseAlbany(url, root_url).parse())
+    return list(ParseAlbany(url, root_url).parse())
 
-    def test_number_of_results(self):
-        assert len(self.results) == 49
 
-    @pytest.mark.parametrize(
-        "idx,start,end",
-        [
-            (0, date(2019, 9, 13), date(2019, 9, 13)),
-            (1, date(2019, 9, 13), date(2019, 9, 22)),
-            (37, date(2020, 1, 1), date(2020, 1, 5)),
-            (48, date(2020, 6, 6), date(2020, 6, 6)),
-        ],
-    )
-    def test_parse_dates(self, idx, start, end):
-        assert self.results[idx].start_date == start
-        assert self.results[idx].end_date == end
+def test_number_of_results(results):
+    assert len(results) == 49
+
+
+@pytest.mark.parametrize(
+    "idx,start,end",
+    [
+        (0, date(2019, 9, 13), date(2019, 9, 13)),
+        (1, date(2019, 9, 13), date(2019, 9, 22)),
+        (37, date(2020, 1, 1), date(2020, 1, 5)),
+        (48, date(2020, 6, 6), date(2020, 6, 6)),
+    ],
+)
+def test_parse_dates(results, idx, start, end):
+    assert results[idx].start_date == start
+    assert results[idx].end_date == end
--- testing/test_parse_hippodrome.py
+++ testing/test_parse_hippodrome.py
@@ -4,33 +4,39 @@ from whatson.ingest import ParseHippodrome
 from datetime import date
 
 
-class TestParseHippodrome:
-    @vcr.use_cassette("fixtures/test_parse_hippodrome.yaml")
-    def setup(self):
-        url = "https://www.birminghamhippodrome.com/whats-on/"
-        root_url = "https://www.birminghamhippodrome.com/"
+@pytest.fixture(scope="session")
+def parser():
+    url = "https://www.birminghamhippodrome.com/whats-on/"
+    root_url = "https://www.birminghamhippodrome.com/"
+    return ParseHippodrome(url, root_url)
 
-        self.cls = ParseHippodrome(url, root_url)
-        self.results = list(self.cls.parse())
 
-    def test_number_of_results(self):
-        assert len(self.results) == 73
+@pytest.fixture(scope="session")
+@vcr.use_cassette("fixtures/test_parse_hippodrome.yaml")
+def results(parser):
+    return list(parser.parse())
 
-    @pytest.mark.parametrize(
-        "idx,start,end",
-        [
-            (0, date(2019, 9, 13), date(2019, 12, 13)),
-            (16, date(2019, 9, 28), date(2019, 9, 28)),
-            (51, date(2019, 12, 21), date(2020, 2, 2)),
-            (52, date(2020, 1, 22), date(2020, 1, 23)),
-            (-1, date(2020, 9, 15), date(2020, 9, 26)),
-        ],
-    )
-    def test_parse_dates(self, idx, start, end):
-        assert self.results[idx].start_date == start
-        assert self.results[idx].end_date == end
 
-    def test_parse_specific_date(self):
-        result = self.cls.date_text_to_date("Wed 8 Apr - Sat 11 Apr 2020")
-        assert result.start == date(2020, 4, 8)
-        assert result.end == date(2020, 4, 11)
+def test_number_of_results(results):
+    assert len(results) == 73
+
+
+@pytest.mark.parametrize(
+    "idx,start,end",
+    [
+        (0, date(2019, 9, 13), date(2019, 12, 13)),
+        (16, date(2019, 9, 28), date(2019, 9, 28)),
+        (51, date(2019, 12, 21), date(2020, 2, 2)),
+        (52, date(2020, 1, 22), date(2020, 1, 23)),
+        (-1, date(2020, 9, 15), date(2020, 9, 26)),
+    ],
+)
+def test_parse_dates(results, idx, start, end):
+    assert results[idx].start_date == start
+    assert results[idx].end_date == end
+
+
+def test_parse_specific_date(results, parser):
+    result = parser.date_text_to_date("Wed 8 Apr - Sat 11 Apr 2020")
+    assert result.start == date(2020, 4, 8)
+    assert result.end == date(2020, 4, 11)
