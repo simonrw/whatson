16cbdbb7d9f442fb4fb0661e61092b98f46e6aeb
Simon Walker <s.r.walker101@googlemail.com>
2019-11-04 21:55:06 +0000

    Include new and old parsers

---
+++ whatson/parsers/base.py
@@ -0,0 +1,169 @@
+from typing import Any, Iterable, NamedTuple
+from ..models import Show
+from ..fetchers import Fetcher
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+from datetime import date
+import re
+
+
+CURRENT_YEAR = date.today().year
+YEAR_RE = re.compile(r"20\d{2}")
+
+
+class DateRange(NamedTuple):
+    start: date
+    end: date
+
+
+class Parser(object):
+    def __init__(self, definition: "TheatreDefinition") -> None:
+        self._defn = definition
+
+    def parse(self, html: str, fetcher: Fetcher) -> Iterable[Show]:
+        while True:
+            soup = BeautifulSoup(html, "lxml")
+            container = soup.select_one(self._defn.container_selector)
+            for elem in container.children:
+                if not isinstance(elem, Tag):
+                    continue
+
+                try:
+                    title = elem.select_one(self._defn.title_selector).text
+
+                    image_url = self._relative_or_absolute_link(
+                        elem.select_one(self._defn.image_selector).attrs["src"]
+                    )
+
+                    link_url = self._relative_or_absolute_link(
+                        elem.select_one(self._defn.link_selector).attrs["href"]
+                    )
+
+                    date_str = elem.select_one(self._defn.date_selector).text.lower()
+                    date = self._date_text_to_date(date_str)
+
+                    if isinstance(date, DateRange):
+                        yield Show(
+                            name=title,
+                            image_url=image_url,
+                            link_url=link_url,
+                            start_date=date.start,
+                            end_date=date.end,
+                        )
+                    else:
+                        yield Show(
+                            name=title,
+                            image_url=image_url,
+                            link_url=link_url,
+                            start_date=date,
+                            end_date=date,
+                        )
+                except AttributeError:
+                    continue
+
+            if self._defn.next_selector is None:
+                # no pagination so just quit
+                return
+            else:
+                next_link = soup.select_one(self._defn.next_selector)
+                if not next_link:
+                    return
+                next_url = next_link.attrs["href"]
+                html = fetcher.fetch(next_url)
+
+
+    def _relative_or_absolute_link(self, link: str) -> str:
+        if self._defn.link_relative:
+            return self._defn.root_url + link
+        else:
+            return link
+
+    def _date_text_to_date(self, date_text, year=None):
+        date_text = date_text.strip().lower()
+        if "-" in date_text or "&" in date_text:
+            if "-" in date_text:
+                parts = date_text.split("-")
+            elif "&" in date_text:
+                parts = date_text.split("&")
+
+            start_year_match = YEAR_RE.search(parts[0])
+            end_year_match = YEAR_RE.search(parts[1])
+
+            if start_year_match:
+                start_year = int(start_year_match.group(0))
+                start = self._date_text_to_date(parts[0], year=start_year)
+
+                if end_year_match:
+                    end = self._date_text_to_date(
+                        parts[1], year=int(end_year_match.group(0))
+                    )
+
+                else:
+                    end = self._date_text_to_date(parts[1], year=start_year)
+            else:
+
+                if end_year_match:
+                    start_year = int(end_year_match.group(0))
+                else:
+                    try:
+                        start_year = year if year is not None else start_year
+                    except UnboundLocalError:
+                        start_year = CURRENT_YEAR
+                start = self._date_text_to_date(parts[0], year=start_year)
+
+                if end_year_match:
+                    end = self._date_text_to_date(
+                        parts[1], year=int(end_year_match.group(0))
+                    )
+                else:
+                    end = self._date_text_to_date(parts[1], year=start_year)
+
+            if start > end:
+                start = date(start.year - 1, start.month, start.day)
+
+            return DateRange(start=start, end=end)
+        else:
+            match = re.search(
+                r"(?P<day_str>\d+)\w*\s*(?P<month_str>\w+)\s*(?P<year_str>\d+)?",
+                date_text,
+            )
+            if not match:
+                raise ValueError("error parsing string {}".format(date_text))
+
+            day = int(match.group("day_str"))
+            month_str = match.group("month_str")
+            month = [
+                "january",
+                "february",
+                "march",
+                "april",
+                "may",
+                "june",
+                "july",
+                "august",
+                "september",
+                "october",
+                "november",
+                "december",
+                "jan",
+                "feb",
+                "mar",
+                "apr",
+                "may",
+                "jun",
+                "jul",
+                "aug",
+                "sep",
+                "oct",
+                "nov",
+                "dec",
+            ].index(month_str) % 12
+
+            if year is None and match.group("year_str") is None:
+                year = CURRENT_YEAR
+
+            if year is not None:
+                return date(year, month + 1, day)
+            else:
+                year = int(match.group("year_str"))
+                return date(year, month + 1, day)
---
+++ whatson/theatredef.py
@@ -0,0 +1,68 @@
+from typing import NamedTuple, Optional, TextIO, List
+import configparser
+from .parsers import Parser, custom as CustomParsers
+
+URL = str
+CSSSelector = str
+Fetcher = str
+
+
+class TheatreDefinition(NamedTuple):
+    """Configuration for a theatre.
+
+    This is typically created from a config file, rather than created manually.
+    """
+
+    name: str
+    active: bool
+    root_url: URL
+    url: URL
+    fetcher: Fetcher
+    custom_class: Optional[str]
+
+    # Whether to treat any links found as relative or not. Defaults to False.
+    link_relative: bool
+
+    # Selectors
+    container_selector: CSSSelector
+    link_selector: CSSSelector
+    image_selector: CSSSelector
+    title_selector: CSSSelector
+    date_selector: CSSSelector
+    next_selector: Optional[CSSSelector]
+
+    @classmethod
+    def parse_config(cls, fptr: TextIO) -> List["TheatreDefinition"]:
+        config = configparser.ConfigParser()
+        config.read_file(fptr)
+        theatres = []
+        for cfg in config.sections():
+            theatre = TheatreDefinition.from_definition(config[cfg])
+            theatres.append(theatre)
+        return theatres
+
+    @classmethod
+    def from_definition(cls, defn: configparser.SectionProxy) -> "TheatreDefinition":
+        return TheatreDefinition(
+            name=defn.name,
+            active=defn.getboolean("active"),
+            root_url=defn["root-url"],
+            url=defn["url"],
+            link_relative=defn.getboolean("link-relative", fallback=False),
+            fetcher=defn["fetcher"],
+            custom_class=defn.get("custom-class", None),
+            container_selector=defn["container-selector"],
+            link_selector=defn["link-selector"],
+            image_selector=defn["image-selector"],
+            title_selector=defn["title-selector"],
+            date_selector=defn["date-selector"],
+            next_selector=defn.get("next-selector"),
+        )
+
+    def to_parser(self) -> Parser:
+        if self.custom_class is not None:
+            cls = getattr(CustomParsers, self.custom_class, None)
+            if not cls:
+                raise ValueError(f"Cannot find custom parser '{self.custom_class}'")
+            return cls(self)
+        return Parser(self)
