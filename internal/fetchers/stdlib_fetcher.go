61fba92ffd4aff9d0b4ac514360097edccf22089
Simon Walker <s.r.walker101@googlemail.com>
2019-11-13 21:47:40 +0000

    Implement initial fetcher using stdlib

---
+++ internal/fetchers/stdlib_fetcher.go
@@ -0,0 +1,31 @@
+package fetcher
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type StdlibFetcher struct {
+}
+
+func NewStdlibFetcher() StdlibFetcher {
+	return StdlibFetcher{}
+}
+
+func (f StdlibFetcher) Fetch(url string) (string, error) {
+	r, err := http.Get(url)
+	if err != nil {
+		log.Printf("error fetching http response from %s\n", url)
+		return "", err
+	}
+	defer r.Body.Close()
+
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("error reading response body")
+		return "", nil
+	}
+
+	return string(b), nil
+}
---
+++ internal/fetchers/stdlib_fetcher_test.go
@@ -0,0 +1,28 @@
+package fetcher
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type handler struct{}
+
+func (handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
+}
+
+func TestFetcher(t *testing.T) {
+	var h handler
+	server := httptest.NewServer(h)
+
+	response, err := NewStdlibFetcher().Fetch(server.URL)
+	if err != nil {
+		t.Errorf("got non-nil response from fetcher: %v\n", err)
+	}
+
+	if response != "ok" {
+		t.Errorf("incorrect response, expected `ok`, got `%s`", response)
+	}
+}
