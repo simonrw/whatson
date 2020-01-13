476d61a188e609d16c777cf7c81a3131a395ddcd
Simon Walker <s.r.walker101@googlemail.com>
2020-01-13 22:51:24 +0000

    Implement test for fetching months

--- testing/test_webapp.py
+++ testing/test_webapp.py
@@ -1,5 +1,31 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring
-from whatson.webapp import interpolate_months
+import pytest
+from whatson.webapp import create_app, interpolate_months
+import datetime
+from unittest import mock
+
+
+@pytest.fixture(scope="module")
+def client(connection):
+    app = create_app(connection)
+    with app.test_client() as client:
+        yield client
+
+
+def test_getting_months(client, cursor):
+    start_date = datetime.date(2019, 1, 2)
+    end_date = datetime.date(2019, 2, 3)
+    cursor.execute(
+        """INSERT INTO shows (theatre, title, image_url, link_url, start_date, end_date)
+            VALUES (%s, %s, %s, %s, %s, %s)""",
+        ("test", "show", "", "", start_date, end_date),
+    )
+
+    rv = client.get("/api/months")
+    data = rv.get_json()
+
+    expected = [{"year": 2019, "month": 1}, {"year": 2019, "month": 2}]
+    assert data["dates"] == expected
 
 
 def test_something():
--- whatson/webapp.py
+++ whatson/webapp.py
@@ -6,77 +6,101 @@ import datetime
 from functools import wraps
 
 
-app = Flask("whatson")
+def create_app(db=None):
+    if db is None:
+        db = DB
 
+    app = Flask("whatson")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
+    class ShowPresenter(object):
+        def __init__(self, show):
+            self.show = show
 
-class ShowPresenter(object):
-    def __init__(self, show):
-        self.show = show
+        def serialise(self):
+            return {
+                "name": self.show["title"],
+                "theatre": self.show["theatre"],
+                "image_url": self.show["image_url"],
+                "link_url": self.show["link_url"],
+                "start_date": self.show["start_date"].isoformat(),
+                "end_date": self.show["end_date"].isoformat(),
+            }
 
-    def serialise(self):
-        return {
-            "name": self.show["title"],
-            "theatre": self.show["theatre"],
-            "image_url": self.show["image_url"],
-            "link_url": self.show["link_url"],
-            "start_date": self.show["start_date"].isoformat(),
-            "end_date": self.show["end_date"].isoformat(),
-        }
+    class ShowEncoder(json.JSONEncoder):
+        def default(self, o):
+            if isinstance(o, ShowPresenter):
+                return o.serialise()
+            else:
+                return super().default(o)
 
+    app.json_encoder = ShowEncoder
 
-class ShowEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ShowPresenter):
-            return o.serialise()
-        else:
-            return super().default(o)
+    # Wrapper decorator that turns any exceptions into JSON messages
+    def json_errors(fn):
+        @wraps(fn)
+        def inner(*args, **kwargs):
+            try:
+                return fn(*args, **kwargs)
+            except Exception as e:
+                return jsonify(status="error", msg=str(e)), 500
 
+        return inner
 
-app.json_encoder = ShowEncoder
+    def jsonify_ok(**kwargs):
+        """Wrapper function to ensure that the `status` key is present in the API result
+        """
+        kwargs.pop("status", None)
+        return jsonify(status="ok", **kwargs)
 
-# Wrapper decorator that turns any exceptions into JSON messages
-def json_errors(fn):
-    @wraps(fn)
-    def inner(*args, **kwargs):
-        try:
-            return fn(*args, **kwargs)
-        except Exception as e:
-            return jsonify(status="error", msg=str(e)), 500
+    @app.route("/api/shows", methods=["POST"])
+    @json_errors
+    def get_by_month():
+        month = int(request.json["month"])
+        year = int(request.json["year"])
 
-    return inner
+        with db as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    """SELECT * FROM shows
+                        WHERE total_months(start_date) <= total_months(%(date_ref)s)
+                        AND total_months(end_date) >= total_months(%(date_ref)s)
+                        ORDER BY start_date ASC
+                        """,
+                    {"date_ref": datetime.date(year, month, 1)},
+                )
+                rows = cursor.fetchall()
 
+        return jsonify_ok(shows=[ShowPresenter(show) for show in rows])
 
-def jsonify_ok(**kwargs):
-    """Wrapper function to ensure that the `status` key is present in the API result
-    """
-    kwargs.pop("status", None)
-    return jsonify(status="ok", **kwargs)
+    @app.route("/api/months", methods=["GET"])
+    @json_errors
+    def get_months():
+        with db as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    """SELECT
+                                EXTRACT(MONTH FROM start_date)::int AS month,
+                                EXTRACT(YEAR FROM start_date)::int AS year
+                            FROM shows
+                            UNION
+                            SELECT
+                                EXTRACT(MONTH FROM end_date)::int AS month,
+                                EXTRACT(YEAR FROM end_date)::int AS year
+                            FROM shows
+                        ORDER BY year, month
+                        """
+                )
+                rows = cursor.fetchall()
 
+        dates = interpolate_months(rows)
 
-@app.route("/api/shows", methods=["POST"])
-@json_errors
-def get_by_month():
-    month = int(request.json["month"])
-    year = int(request.json["year"])
+        return jsonify_ok(dates=list(dates))
 
-    with DB as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(
-                """SELECT * FROM shows
-                    WHERE total_months(start_date) <= total_months(%(date_ref)s)
-                    AND total_months(end_date) >= total_months(%(date_ref)s)
-                    ORDER BY start_date ASC
-                    """,
-                {"date_ref": datetime.date(year, month, 1)},
-            )
-            rows = cursor.fetchall()
-
-    return jsonify_ok(shows=[ShowPresenter(show) for show in rows])
+    return app
 
 
 def interpolate_months(seen_months):
@@ -104,26 +128,4 @@ def interpolate_months(seen_months):
             current_year += 1
 
 
-@app.route("/api/months", methods=["GET"])
-@json_errors
-def get_months():
-    with DB as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(
-                """SELECT
-                            EXTRACT(MONTH FROM start_date)::int AS month,
-                            EXTRACT(YEAR FROM start_date)::int AS year
-                        FROM shows
-                        UNION
-                        SELECT
-                            EXTRACT(MONTH FROM end_date)::int AS month,
-                            EXTRACT(YEAR FROM end_date)::int AS year
-                        FROM shows
-                    ORDER BY year, month
-                    """
-            )
-            rows = cursor.fetchall()
-
-    dates = interpolate_months(rows)
-
-    return jsonify_ok(dates=list(dates))
+app = create_app()
